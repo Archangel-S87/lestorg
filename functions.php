32d096285747b73455f434f2cff7479a30fde1b5
Sergey <siarheisluka@gmail.com>
2020-02-05 02:49:22 +0300

    Сброс

--- functions.php
+++ functions.php
@@ -6,8 +6,6 @@ require_once 'libs/tgm/tgm.php';
 
 require_once 'inc/functions-templates.php';
 
-require_once 'woocommerce/functions.php';
-
 include_once 'inc/Lestorg.php';
 
 function LT() : Lestorg {
@@ -63,6 +61,61 @@ function get_tag_attr($attrs = [], $echo = true) {
     }
 
     return $str;
+}
+
+// Получает thumbnail src продукта
+function lestorg_woocommerce_get_thumbnail_image($size = 'woocommerce_thumbnail', $placeholder = true) {
+    global $product;
+    if (!$product instanceof WC_Product) return null;
+
+    $size = apply_filters('single_product_archive_thumbnail_size', $size);
+
+    $image = null;
+    if ($product->get_image_id()) {
+        $image = wp_get_attachment_image_url($product->get_image_id(), $size);
+    } elseif ($product->get_parent_id()) {
+        $parent_product = wc_get_product($product->get_parent_id());
+        if ($parent_product) {
+            $image = wp_get_attachment_image_url($parent_product->get_image_id(), $size);
+        }
+    }
+
+    if (!$image && $placeholder) {
+        $image = wc_placeholder_img_src($size);
+    }
+
+    return apply_filters('lestorg_product_get_image', $image, $product, $size, $image, $placeholder);
+}
+
+function set_product_loop_in_swiper($attrs) {
+    $classes = $attrs['class'] ?? [];
+    $classes = remove_value_array('catalog-grid__col', $classes);
+    $classes[] = 'swiper-slide';
+    $attrs['class'] = $classes;
+    return $attrs;
+}
+
+function get_top_parent_id_product_cat($category_id) {
+    do {
+        $cat = get_term($category_id, 'product_cat');
+        $category_id = $cat->parent;
+        $category_parent_id = $cat->term_id;
+    } while ($category_id);
+    return $category_parent_id;
+}
+
+/*
+ * Находит товар в карзине по переданому id
+ */
+function find_cart_item_by_id($product_id) {
+    $cart_item_key = '';
+    foreach (WC()->cart->get_cart() as $key => $item) {
+        if ($item['product_id'] == $product_id) {
+            $cart_item_key = $key;
+            break;
+        }
+    }
+    return $cart_item_key;
 }
 
 
